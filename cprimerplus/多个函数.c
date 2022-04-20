51d5a283573ffbbfe8e87ca5abf793637bfa340e
lz <kidsde1314@gmail.com>
2022-04-20 19:19:02 +0800

    多个函数

---
+++ cprimerplus/多个函数.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+void butler(void);
+int main ()
+{
+printf("i will summon the butler fuction.\n");
+butler();
+printf("yes.\n");
+return 0;
+}
+
+void butler (void)
+{
+   printf("you rang, sir?\n"); 
+
+}
